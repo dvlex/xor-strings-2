98973c3a912d8d0ae690cb1cf0220f8ad3e873a9
Alex Castillo <alex@lexdrel.com>
2024-10-09 19:21:05 +0000

    final solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		string s = Console.ReadLine();
+		string t = Console.ReadLine();
+		Console.WriteLine(StringsXor(s, t));
+	}
+
+	static string StringsXor(string s, string t)
+	{
+		char[] result = new char[s.Length];
+		for (int i = 0; i < s.Length; i++)
+		{
+			result[i] = s[i] == t[i] ? '0' : '1';
+		}
+		return new string(result);
+	}
+}
